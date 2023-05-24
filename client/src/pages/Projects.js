da13d8bb1735dba927d4020f2d4fe478aeb666fa
jackp <jackpetersenmusic@gmail.com>
2023-05-24 08:32:46 -0700

    deleted attributes

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -66,7 +66,7 @@ const Projects = () => {
             <div className="video-container">
                 <VideoDiv>
                   <StyledVideo autoPlay loop muted id='video'>
-                      <source src='thehoopscoop2.mp4' type='video/mp4' />
+                      <source src='thehoopscoop3.mp4' type='video/mp4' />
                   </StyledVideo>
                 </VideoDiv>
                 <TextDiv>
@@ -130,7 +130,7 @@ const Projects = () => {
             <div className="video-container">
                 <VideoDiv>
                   <StyledVideo autoPlay loop muted id='video'>
-                      <source src='kudos6.mp4' type='video/mp4' />
+                      <source src='kudos7.mp4' type='video/mp4' />
                   </StyledVideo>
                 </VideoDiv>
 
